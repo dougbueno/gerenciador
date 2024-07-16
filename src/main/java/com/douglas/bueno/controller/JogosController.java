7ad1deefebf547a280f971fe745165fb14be50e9
Douglas Bueno <douglas.bueno.oliveira@gmail.com>
2024-07-16 10:01:44 -0300

    atualizacao de amistosos

--- src/main/java/com/douglas/bueno/controller/JogosController.java
+++ src/main/java/com/douglas/bueno/controller/JogosController.java
@@ -1,10 +1,13 @@
 package com.douglas.bueno.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -45,6 +48,33 @@ public class JogosController {
 
 		amistosoService.atualizarAtivos(jogosAtivos);
 
+		return ResponseEntity.ok().build();
+	}
+
+	@PutMapping("/amistoso/{id}")
+	public ResponseEntity<?> atualizarJogoAmistoso(@PathVariable Long id,@RequestBody Jogos jogos) {
+		Optional<Jogos> optionalJogos = amistosoRepository.findById(id);
+
+		if (!optionalJogos.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		Jogos existingJogos = optionalJogos.get();
+		existingJogos.setGolsCasa(jogos.getGolsCasa());
+		existingJogos.setGolsVisitante(jogos.getGolsVisitante());
+		amistosoRepository.save(existingJogos);
+		return ResponseEntity.ok(existingJogos);
+	}
+
+	@DeleteMapping("/amistoso/{id}")
+	public ResponseEntity<?> deleteJogos(@PathVariable Long id) {
+		Optional<Jogos> optionalJogos = amistosoRepository.findById(id);
+
+		if (!optionalJogos.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		amistosoRepository.deleteById(id);
 		return ResponseEntity.ok().build();
 	}
 
